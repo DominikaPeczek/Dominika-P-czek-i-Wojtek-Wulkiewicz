6e182ad8bb4c99ec9b0a4208b5158417b3ba52d7
DominikaPeczek <117264361+DominikaPeczek@users.noreply.github.com>
2025-02-13 07:08:26 +0100

    Add files via upload

---
+++ Konto.cs
@@ -0,0 +1,29 @@
+﻿using konto;
+var customers = new List<Customer>();
+customers.Add(new Customer("1", "Andrzej", 5000));
+customers.Add(new Customer("2", "Bartek",1000));
+customers.Add(new Customer("3", "Kuba", 2500));
+
+Console.WriteLine("Podaj ID klienta");
+var id = Console.ReadLine();
+var selectedCustomer = customers.FirstOrDefault(c => c.Id == id);
+
+if (selectedCustomer != null)
+{
+    Console.WriteLine("Podaj kwotę przelewu");
+    var amountText = Console.ReadLine();
+    var amount = decimal.Parse(amountText);
+    if (amount <= selectedCustomer.Saldo)
+    {
+        Console.WriteLine("Podaj odbiorcę przelewu");
+        var pid = Console.ReadLine();
+        var paymentCustomer = customers.FirstOrDefault(c => c.Id == pid);
+        if (paymentCustomer != null)
+        {
+            selectedCustomer.Saldo = selectedCustomer.Saldo - amount;
+            paymentCustomer.Saldo = paymentCustomer.Saldo + amount;
+        }
+    }
+}
+ Console.ReadLine();
+
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konto
+{
+    internal class Customer
+    {
+        public Customer(string id, string name, decimal saldo)
+        {
+            Id = id;
+            Name = name;
+            Saldo = saldo;
+        }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public decimal Saldo { get; set; }
+
+    }
+
+
+}
+
+
